e6c0f5642598d35f5a3c1f971cd31e36b1a20b4c
Matthew West <mattywest@ntlworld.com>
2015-03-30 16:45:29 +0100

    Fixed Reverts

--- app/src/main/java/com/thrive/westm10/thrive/MainActivity.java
+++ app/src/main/java/com/thrive/westm10/thrive/MainActivity.java
@@ -1,17 +1,45 @@
 package com.thrive.westm10.thrive;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private String[] mNavigationDrawerItemTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNavigationDrawerItemTitles= getResources().getStringArray(R.array.navigation_drawer_items_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        ObjectDrawerItem[] drawerItem = new ObjectDrawerItem[4];
+
+        drawerItem[0] = new ObjectDrawerItem(R.drawable.test_icon, "Profile");
+        drawerItem[1] = new ObjectDrawerItem(R.drawable.test_icon, "Nutrition");
+        drawerItem[2] = new ObjectDrawerItem(R.drawable.test_icon, "Fitness");
+        drawerItem[3] = new ObjectDrawerItem(R.drawable.test_icon, "Settings");
+
+        DrawerItemCustomAdapter adapter = new DrawerItemCustomAdapter(this, R.layout.listview_item_row, drawerItem);
+
+        mDrawerList.setAdapter(adapter);
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
     }
 
 
@@ -35,5 +63,51 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+
+    }
+
+    private void selectItem(int position) {
+
+        Fragment fragment = null;
+
+        switch (position) {
+            case 0:
+                fragment = new ProfileFragment();
+                break;
+            case 1:
+                fragment = new NutritionFragment();
+                break;
+            case 2:
+                fragment = new FitnessFragment();
+                break;
+            case 3:
+                fragment = new SettingsActivity();
+                break;
+
+            default:
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+            mDrawerList.setItemChecked(position, true);
+            mDrawerList.setSelection(position);
+            //getActionBar().setTitle(mNavigationDrawerItemTitles[position]);
+            mDrawerLayout.closeDrawer(mDrawerList);
+
+        } else {
+            Log.e("MainActivity", "Error in creating fragment");
+        }
     }
 }
---
+++ app/src/main/java/com/thrive/westm10/thrive/ObjectDrawerItem.java
@@ -0,0 +1,16 @@
+package com.thrive.westm10.thrive;
+
+/**
+ * Created by Matthew West on 30/03/2015.
+ */
+public class ObjectDrawerItem {
+    public int icon;
+    public String name;
+
+    // Constructor.
+    public ObjectDrawerItem(int icon, String name) {
+
+        this.icon = icon;
+        this.name = name;
+    }
+}
